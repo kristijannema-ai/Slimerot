199877df3216f74c057c8f316847be0e3700e07a
Kristijan Nema <kristijan.nema@tfzr.rs>
2026-09-11 01:14:38 +0200

    Add scripts/managers/SlimerotWorldManager.gd for Slimerot foundation

---
+++ scripts/managers/SlimerotWorldManager.gd
@@ -0,0 +1,40 @@
+extends Node
+
+signal zone_changed(zone_id: int)
+signal respawn_requested
+var structures: Dictionary = {}
+
+func _ready() -> void:
+	for data in SlimerotData.structures():
+		structures[data.id] = data
+
+func travel(zone_id: int) -> bool:
+	if zone_id < 0 or zone_id > 1 or zone_id > GameState.highest_zone_unlocked:
+		return false
+	GameState.current_zone = zone_id
+	GameState.player_hp = SkillTreeManager.derived_stats().max_hp
+	zone_changed.emit(zone_id)
+	GameState.changed.emit()
+	GameState.critical_change.emit("zone_transition")
+	return true
+
+func repair(id: String) -> bool:
+	var data: SlimerotData.StructureData = structures.get(id)
+	if data == null or GameState.current_zone != data.zone or GameState.structure_unlocked_flags.get(data.unlock_flag, false):
+		return false
+	if not GameState.spend("Coins", data.coin_cost):
+		return false
+	GameState.structure_unlocked_flags[data.unlock_flag] = true
+	GameState.changed.emit()
+	GameState.critical_change.emit("structure_purchase")
+	return true
+
+func record_kill(zone_id: int, coins: int) -> void:
+	var key := str(zone_id)
+	GameState.zone_kill_counts[key] = int(GameState.zone_kill_counts.get(key, 0)) + 1
+	GameState.award_coins(coins)
+
+func respawn() -> void:
+	GameState.player_hp = SkillTreeManager.derived_stats().max_hp
+	respawn_requested.emit()
+	GameState.changed.emit()
